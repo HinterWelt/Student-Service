353cf5d35351882b62999c99d5674d73fd01c9e7
bilbo <bilbo@localhost>
2017-09-19 15:09:42 -0500

    Initial Commit.

--- src/main/java/com/hinterwelt/student/dao/MoodleStudentDaoImpl.java
+++ src/main/java/com/hinterwelt/student/dao/MoodleStudentDaoImpl.java
@@ -1,29 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.hinterwelt.student.dao;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hinterwelt.student.entity.ErrorMessage;
 import com.hinterwelt.student.entity.Student;
 import com.hinterwelt.student.entity.Users;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Collection;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
@@ -33,11 +18,20 @@ import org.springframework.web.client.RestTemplate;
  * @author bilbo
  */
 @Repository("moodle")
+@ConfigurationProperties
+@PropertySource("classpath:secure.properties")
 public class MoodleStudentDaoImpl implements StudentDao {
+    @Value("${restMoodleURI}")
+    private String restMoodleURI;
+    @Value("${vletoken}")
+    private String vletoken;
+    @Value("${requestFormat}")
+    private String requestFormat;
 
     @Override
     public List<Student> getAllStudents() {
-        String reqURL = "http://moodle.hinterwelt.org/webservice/rest/server.php?wstoken=4143ab05998b499b05d06fb46e674915&wsfunction=core_user_get_users&moodlewsrestformat=json&criteria[0][key]=role&criteria[0][value]=student";
+        String moodleFunction = "&wsfunction=core_user_get_users&criteria[0][key]=role&criteria[0][value]=student";
+        String reqURL = restMoodleURI + vletoken + moodleFunction + requestFormat;
         RestTemplate restTemplate = new RestTemplate();
  
         List<Student> students = restTemplate.getForObject(reqURL,Users.class).getStudents();
@@ -49,7 +43,8 @@ public class MoodleStudentDaoImpl implements StudentDao {
     @Override
     public Student getStudentById(int id) {
         RestTemplate restTemplate = new RestTemplate();
-        String reqURL = "http://moodle.hinterwelt.org/webservice/rest/server.php?wstoken=4143ab05998b499b05d06fb46e674915&wsfunction=core_user_get_users_by_field&field=id&values[0]=" + id + "&moodlewsrestformat=json";
+        String moodleFunction = "&wsfunction=core_user_get_users_by_field&field=id&values[0]=" + id;
+        String reqURL = restMoodleURI + vletoken + moodleFunction + requestFormat;
 
         ResponseEntity<List<Student>> studentResponse
                 = restTemplate.exchange(reqURL,
@@ -62,7 +57,8 @@ public class MoodleStudentDaoImpl implements StudentDao {
 
     @Override
     public ErrorMessage removeStudentById(int id) {
-        String reqURL = "http://moodle.hinterwelt.org/webservice/rest/server.php?wstoken=4143ab05998b499b05d06fb46e674915&wsfunction=core_user_delete_users&moodlewsrestformat=json&criteria[0][key]=id&criteria[0][value]="+id;
+        String moodleFunction = "&wsfunction=core_user_delete_users&criteria[0][key]=id&criteria[0][value]="+id;
+        String reqURL = restMoodleURI + vletoken + moodleFunction + requestFormat;
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.delete(reqURL);
 
@@ -77,11 +73,15 @@ return eResponse;
 
     @Override
     public void updateStudentById(Student student) {
+        String moodleFunction = "&wsfunction=core_user_delete_users&criteria[0][key]=id&criteria[0][value]=";
+        String reqURL = restMoodleURI + vletoken + moodleFunction + requestFormat;
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void createStudent(Student student) {
+        String moodleFunction = "&wsfunction=core_user_delete_users&criteria[0][key]=id&criteria[0][value]=";
+        String reqURL = restMoodleURI + vletoken + moodleFunction + requestFormat;
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
